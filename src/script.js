020815130aac8e1dd3f23e404b0020eb9211170c
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-06-25 16:55:55 -0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,151 @@
+import * as dat from 'dat.gui';
+import * as THREE from 'three';
+import './style.css';
+//Texture Loader
+const textureLoader = new THREE.TextureLoader();
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+const geometry = new THREE.PlaneBufferGeometry(1, 1.3);
+
+// Objects
+for (let i = 0; i < 4; i++) {
+  const material = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(`/photographs/${i}.jfif`),
+  });
+
+  const img = new THREE.Mesh(geometry, material);
+
+  img.position.set(Math.random()+0.003, i * -1.8);
+
+  scene.add(img);
+}
+
+let objs = [];
+
+scene.traverse((object) => {
+    if (object.isMesh) {
+        objs.push(object)
+        
+    }
+})
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
+
+gui.add(camera.position, 'y').min(-5).max(10);
+gui.add(camera.position, 'x').min(-5).max(10);
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+//Moust
+window.addEventListener('wheel', onMouseWheel);
+
+let y = 0;
+let position = 0;
+
+function onMouseWheel(event) {
+  y = -event.deltaY * 0.0005;
+}
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 + 1;
+    mouse.y = -(event.clientY / sizes.height) *2 - 1;
+})
+/**
+ * Animate
+ */
+
+const raycaster = new THREE.Raycaster();
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  position += y;
+  y *= .9;
+
+  //Raycaster
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(objs);
+    console.log(intersects)
+    for (const intersect of intersects) {
+        
+    }
+
+
+
+  camera.position.y = position;
+
+  // Update Orbital Controls
+  // controls.update()
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
